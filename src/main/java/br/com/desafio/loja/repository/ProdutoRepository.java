2a42f4677db7cf66ec5a9dcb4095b191fd6a4483
FernandesLr <leo1ramosf@gmail.com>
2025-06-06 20:42:41 -0300

    feat: adiciona ProdutoRepository com métodos cadastrar e buscarPorId

---
+++ src/main/java/br/com/desafio/loja/repository/ProdutoRepository.java
@@ -0,0 +1,25 @@
+package br.com.desafio.loja.repository;
+
+import br.com.desafio.loja.model.Produto;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class ProdutoRepository {
+    private List<Produto> produtos = new ArrayList<>();
+
+    public void salvar(Produto produto) {
+        produtos.add(produto);
+    }
+
+
+
+    public Produto buscarPorId(Long id) {
+        return produtos.stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+}
