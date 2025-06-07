8a356d98ca651287261b57f9a3a7cdfd30d32ee3
Leonardo Fernandes Ramos <140019222+FernandesLR@users.noreply.github.com>
2025-06-06 22:41:26 -0300

    Update ProdutoRepository.java
    Renomeia o método cadastrar

--- src/main/java/br/com/desafio/loja/repository/ProdutoRepository.java
+++ src/main/java/br/com/desafio/loja/repository/ProdutoRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class ProdutoRepository {
     private List<Produto> produtos = new ArrayList<>();
 
-    public void salvar(Produto produto) {
+    public void cadastrar(Produto produto) {
         produtos.add(produto);
     }
 
