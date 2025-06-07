dd936ccb001cc6aafe90c9a6e7c2a287bee0dc8c
FernandesLr <leo1ramosf@gmail.com>
2025-06-06 21:09:10 -0300

    feat: Adiciona UsuarioRepository e o método de cadastrar

---
+++ src/main/java/br/com/desafio/loja/repository/UsuarioRepository.java
@@ -0,0 +1,19 @@
+package br.com.desafio.loja.repository;
+
+import br.com.desafio.loja.model.Usuario;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class UsuarioRepository {
+
+    private final List<Usuario> usuarios = new ArrayList<>();
+
+    public void salvar(Usuario usuario) {
+        usuarios.add(usuario);
+    }
+
+
+}
