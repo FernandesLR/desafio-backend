66e0c98052d646d034d86dc295c87c95d32d8415
FernandesLr <leo1ramosf@gmail.com>
2025-06-06 21:10:28 -0300

    feat: Adiciona VendaRepository e os métodos de cadastrar e mostrar todos

---
+++ src/main/java/br/com/desafio/loja/repository/VendaRepository.java
@@ -0,0 +1,20 @@
+package br.com.desafio.loja.repository;
+
+import br.com.desafio.loja.model.Venda;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class VendaRepository {
+    private List<Venda> vendas = new ArrayList<>();
+
+    public void salvar(Venda venda) {
+        vendas.add(venda);
+    }
+
+    public List<Venda> listarTodas() {
+        return vendas;
+    }
+}
